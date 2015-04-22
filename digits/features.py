31753390427799d8ef505c938c64fa08d663a514
Lucas Valenzuela <luvalenz@uc.cl>
2015-04-22 14:13:25 -0300

    primer avance

--- digits/features.py
+++ digits/features.py
@@ -1,3 +1,9 @@
+from skimage.feature import hog
+from skimage import color
+from skimage.transform import resize
+
+import numpy as np
+
 def get_4c_feats(image):
   pass
 
@@ -7,5 +13,8 @@ def get_8c_feats(image):
 def get_mixed_feats(image):
   pass
 
-def get_hog_feats(image):
-  pass
+def get_hog_feats(image, bins, pixels_per_cell_side, cells_per_block_side):
+  image_resized = resize(color.rgb2gray(image),(64,64))
+  fd = hog(image_resized, orientations=bins, pixels_per_cell=(pixels_per_cell_side, pixels_per_cell_side),
+                        cells_per_block=(cells_per_block_side, cells_per_block_side))
+  return fd
--- digits/main.py
+++ digits/main.py
@@ -1,76 +1,98 @@
 import numpy as np
 import features
-from classifier import Classifier
+import glob
+from scipy import misc
+import re
+from sklearn.svm import SVC
 
-# Returns a data structure containing a model for classification
-def train(feats_vec):
-  model = []
-  return model
+###HOG OPTIONS
+bins = 4
+side_pixels_per_cell = 2
+side_cells_per_block = 1
 
-# Returns an np matrix with the information of the confusion matrix
-def get_confusion_matrix(results):
-  global classes_count
-  matrix = np.zeros((classes_count, classes_count))
-  return matrix
+def get_class_MNIST(file_path):
+    class_str = re.split('\-|\_|\.',file_path)[-2]
+    return int(class_str)
 
-# Returns the precision using the confusion matrix
-def get_precision(confusion):
-  return precision
+def get_class_CVL(file_path):
+    class_str = file_path.split('/')[-1].split('-')[0]
+    return int(class_str)
+
+
+#0 MNIST
+#1 CVL
+
+#0 train
+#1 test
+def get_file_list(dataset_option, train_or_test):
+  if dataset_option == 0 and train_or_test == 0:
+    return glob.glob('Images_MNIST/Train/*')
+  elif dataset_option == 0 and train_or_test == 1:
+    return glob.glob('Images_MNIST/Test/*')
+  elif dataset_option == 1 and train_or_test == 0:
+    return glob.glob('Images_CVL/train/*')
+  else:
+    return glob.glob('Images_CVL/test/*')
+
+
+def get_dataset(feats_option, dataset_option, train_or_test):
+  feats = None
+  classes = []
+  for file_path in file_list:
+    image = misc.imread(file_path)
+    ##  get features
+    if feats_option == 1:
+      img_feats = features.get_4c_feats(image)
+    elif feats_option == 2:
+      img_feats = features.get_8c_feats(image)
+    elif feats_option == 3:
+      img_feats = features.get_mixed_feats(image)
+    else:
+      img_feats = features.get_hog_feats(image,bins,side_pixels_per_cell,side_cells_per_block)
+    if feats is None:
+      feats = img_feats
+    else:
+      training_feats = np.vstack((feats,img_feats))
+
+      classes.append(get_class_from_file_path(file_path))
+
+    return (feats, np.array(classes))
 
 ################################################################################
 #             MAIN
 #-------------------------------------------------------------------------------
 def main():
   # Read training images
-  features_vector = []
+
+
+  feats_option = input()
+
+
   print ("Choose an option for features: \n"\
          "[1] 4c\n"\
          "[2] 8c\n"\
          "[3] 4c concatenated with 8c\n"\
          "[4] HOG"
   )
-  feats_option = input()
+
+  training_file_list = glob.glob("Images_CVL/train/*")[0:9]
+  testing_file_list = glob.glob("Images_CVL/test/*")
+  # training_file_list = glob.glob("Images_MNIST/Train/*")
+  # testing_file_list = glob.glob("Images_MNIST/Test/*")
+
 
   ##############################################################################
   ###########                      Training                          ###########
   ##############################################################################
 
-  # for each image in training:
-  ##  get features
-  if feats_option == 1:
-    img_feats = features.get_4c_feats(img)
-  elif feats_option == 2:
-    img_feats = features.get_8c_feats(img)
-  elif feats_option == 3:
-    img_feats = features.get_mixed_feats(img)
-  else:
-    img_feats = features.get_hog_feats(img)
+  training_feats, training_classes = get_dataset(training_file_list)
 
-  features_vector.append(img_feats)
-
-  model = train(features_vector)
-  classifier = Classifier(model)
 
   ##############################################################################
   ###########                      Testing                           ###########
   ##############################################################################
-  
-  testing_feats = []
-  # for each image in testing
-  if feats_option == 1:
-    img_feats = features.get_4c_feats(img)
-  elif feats_option == 2:
-    img_feats = features.get_8c_feats(img)
-  elif feats_option == 3:
-    img_feats = features.get_mixed_feats(img)
-  else:
-    img_feats = features.get_hog_feats(img)
-  
-  testing_feats.append(img_feats)
-  results = classifier.evaluate(testing_feats)
 
-  confusion = 
-
+  testing_feats, testing_classes = get_dataset(training_file_list)
 
 if __name__ == '__main__':
   main()
