3477f4c2bfe06c032e7cc7a0d865e9f1b5133118
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-05-21 16:13:32 -0300

    usando kmeans de scikit learn

--- img-retrieval/main.py
+++ img-retrieval/main.py
@@ -11,13 +11,19 @@ def main():
 	
 	# Training
 	#---------------------------------------------------------------------------
+	# Extracting descriptors
 	start = time()
 	descriptors = train.get_descriptors(img_files[0:100])
 	end = time()
 	elapsed_time = utils.humanize_time(end - start)
 	print("Elapsed time getting the descriptors {0}.".format(elapsed_time))
+	# Clustering
 	k = 64
+	start = time()
 	clusters = train.get_clusters(k, descriptors)
+	end = time()
+	elapsed_time = utils.humanize_time(end - start)
+	print("Elapsed time clustering for k={0} {1}".format(k, elapsed_time))
 	np.savetxt("clusters64.csv", clusters, delimiter=",")
 	
 	# Testing
