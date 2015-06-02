61812a10a19b73f783ff26d13d0fd2f8837eb618
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-02 00:05:13 -0300

    ahora se deberia poder obtener los descriptores para todas las clases (en training)

--- animal-classification/main.py
+++ animal-classification/main.py
@@ -7,35 +7,54 @@ import numpy as np
 from dataset import Dataset
 import cPickle as pickle
 
-start = time.time()
-# Define the training and testing sets
-# path = "dataset"
-# dataset = Dataset(path)
-# dataset.generate_sets()
-# pickle.dump(dataset, open("dataset.obj", "wb"), protocol=2)
-dataset = pickle.load(open("dataset.obj", "rb"))
+def main():
 
-# Get SIFT descriptors per class
-classes = dataset.get_classes()
-train_set = dataset.get_train_set()
-# Only using the first class
-class_des = None
-step = (len(train_set[0]) * 5) / 100
-for i in range(len(train_set[0])):
-	if i % step == 0:
-		percentage = (i * 100) / len(train_set[0])
-		print("Getting SIFT from image {0} of {1}({2}%) ...".format(
-				i, len(train_set[0]), percentage
+	start = time.time()
+	# Define the training and testing sets
+	# path = "dataset"
+	# dataset = Dataset(path)
+	# dataset.generate_sets()
+	# pickle.dump(dataset, open("dataset.obj", "wb"), protocol=2)
+	dataset = pickle.load(open("dataset.obj", "rb"))
+
+	# Get SIFT descriptors per class
+	classes = dataset.get_classes()
+	train_set = dataset.get_train_set()
+	for i in range(1, len(classes)):
+		class_name = classes[i]
+		class_files = train_set[i]
+		print("Getting descriptors for class {0} of length {1}".format(
+				class_name, len(class_files)
 			)
 		)
-	img = cv2.imread(path)
-	kp, des = descriptors.sift(img)
-	if class_des == None:
-		class_des = np.array(des, dtype=np.uint16)
-	else:
-		class_des = np.vstack((class_des, np.array(des, dtype=np.uint16)))
-print("descriptors of shape = {0}".format(class_des.shape))
-filename = "train/des_" + classes[0] + ".obj"
-pickle.dump(class_des, open(filename, "wb"), protocol=2)
-end = time.time()
-print("Elapsed time processing {0}".format(utils.humanize_time(end - start)))
+		store_descriptors(class_files, class_name)
+	end = time.time()
+	print("Elapsed time processing {0}".format(
+			utils.humanize_time(end - start)
+		)
+	)
+
+
+def store_descriptors(filenames, class_name):
+	class_des = None
+	step = (len(filenames) * 5) / 100
+	for i in range(len(filenames)):
+		path = filenames[i]
+		if i % step == 0:
+			percentage = (i * 100) / len(filenames)
+			print("Getting SIFT from image {0} of {1}({2}%) ...".format(
+					i, len(filenames), percentage
+				)
+			)
+		img = cv2.imread(path)
+		kp, des = descriptors.sift(img)
+		if class_des == None:
+			class_des = np.array(des, dtype=np.uint16)
+		else:
+			class_des = np.vstack((class_des, np.array(des, dtype=np.uint16)))
+	print("descriptors of shape = {0}".format(class_des.shape))
+	filename = "train/des_" + class_name + ".obj"
+	pickle.dump(class_des, open(filename, "wb"), protocol=2)
+
+if __name__ == '__main__':
+	main()
