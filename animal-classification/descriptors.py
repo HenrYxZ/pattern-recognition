fe7aa9718c1f49b06c2cf28c531f33d3393c9acb
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-08 16:31:07 -0300

    testeado

--- animal-classification/descriptors.py
+++ animal-classification/descriptors.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 import cPickle as pickle
 import numpy as np
+import time
 
 def sift(img):
 	''' Gets a list of 128 - dimensional descriptors using SIFT and DoG
@@ -46,27 +47,35 @@ def counts():
 	return counts
 
 def random_sample(counts):
+	n = counts[-1]
 	sample_size = 100000
 	sample_indices = np.random.choice(n, sample_size, replace=False)
 	sample_indices.sort()
 	des_files = glob.glob("train/*")
 	counter = 0
+	counter_changed = False
 	sample = []
 	# This is the current descriptors list of a class
 	current_des = None
+	previous_classes_len = 0
 	for sample_index in sample_indices:
-		if sample_index < counts[counter]:
+		if sample_index < previous_classes_len + counts[counter]:
 			if current_des is None:
 				current_des = pickle.load(open(des_files[counter], "rb"))
 		else:
+			previous_classes_len += counts[counter]
+			counter_changed = True
 			counter += 1
 			current_des = pickle.load(open(des_files[counter], "rb"))
-		previous_classes_len = 0
-		for i in range(len(counter) - 1):
-			previous_classes_len += counter[i]
 		# The index for the descriptor in the current class (sum the lengths
 		# of the previous classes)
 		current_class_idx = sample_index - previous_classes_len
+		if counter_changed:
+			time.sleep(2)
+			print("previous_classes_len = {0}".format(previous_classes_len))
+			print("current_class_idx = {0}".format(current_class_idx))
+			print("sample index = {0}".format(sample_index))
+			print("counter = {0}".format(counter))
 		sample.append(current_des[current_class_idx])
 	return np.array(sample)
 
--- animal-classification/test_script.py
+++ animal-classification/test_script.py
@@ -1,4 +1,12 @@
 import descriptors
 
 # Testing counts
-print("counts = {0}".format(descriptors.counts()))
+# counts = descriptors.counts()
+# print("counts = {0}".format(counts))
+counts = [
+	1085495, 761595, 1035142, 1195164, 721518, 950140, 1125565, 1005910, 845804,
+	820759, 9547092
+]
+
+# Testing random sample
+sample = descriptors.random_sample(counts)
