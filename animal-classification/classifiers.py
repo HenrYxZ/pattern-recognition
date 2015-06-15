1acf6531280d039a9a15fd9fd4702bc31728b60d
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-15 11:10:58 -0300

    prueba chica con knn

--- animal-classification/classifiers.py
+++ animal-classification/classifiers.py
@@ -6,7 +6,8 @@ import utils
 import scipy.io as sio
 
 def knn(dataset):
-	n_classes = len(dataset.get_classes())
+	# n_classes = len(dataset.get_classes())
+	n_classes = 5
 	des_files = glob.glob("train/*.mat")
 	predictions = []
 	counter = 0
@@ -19,12 +20,17 @@ def knn(dataset):
 		data = sio.loadmat(fname)
 		class_des = data["stored"]
 
-		sample_size = 30000
+		sample_size = 1000
 		current_sample = utils.random_sample(class_des, sample_size)
 		class_des = None
 		classes_sample.append(current_sample)
 
 	for test_files in dataset.get_test_set():
+		n_files = 15
+		# TESTING! #
+		if counter == n_classes:
+			break
+		############
 		print("Starting to predict the test set of class {0}.".format(counter))
 		# iteration over classes
 		predictions.append([])
@@ -33,7 +39,7 @@ def knn(dataset):
 		test_des_list = []
 		# step = (len(test_files) * 5) / 100
 		# for i in range(len(test_files)):
-		for i in range(30):
+		for i in range(n_files):
 			# iteration over files inside a class
 			# if i % step == 0:
 			# 	percentage = (i * 100) / len(test_files)
@@ -49,16 +55,16 @@ def knn(dataset):
 		# We can't add more descriptors because it would be too expensive in RAM
 
 		# distances = np.zeros((len(test_files), n_classes))
-		distances = np.zeros((30, n_classes))
-		for c in n_classes:
+		distances = np.zeros((n_files, n_classes))
+		for c in range(n_classes):
 			# for img_index in range(len(test_files)):
-			for img_index in range(30):
+			for img_index in range(n_files):
 				des = test_des_list[img_index]
 				print("Getting dist for img index = {0}".format(img_index))
 				class_des = classes_sample[c]
 				distances[img_index][c] = dist_nn_class(des, class_des)
 		# for img_index in range(len(test_files)):
-		for img_index in range(30):
+		for img_index in range(n_files):
 			predictions[-1].append(np.argmin(distances[img_index]))
 		counter += 1
 	return predictions
--- animal-classification/test_script.py
+++ animal-classification/test_script.py
@@ -2,6 +2,10 @@ import descriptors
 import cPickle as pickle
 import dataset
 import classifiers
+import time
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Testing counts
 def test_counts():
@@ -18,15 +22,36 @@ def test_sample():
 
 # Testing knn
 def test_knn():
-	dataset = pickle.load(open("dataset.obj", "rb"))
-	predictions = classifiers.knn(dataset)
-	print("predictions 10x10 = {0}".format(predictions[:10][:10]))
-	hist = np.zeros((10, 10), dtype=np.uint32)
+	# dataset = pickle.load(open("dataset.obj", "rb"))
+	# start = time.time()
+	# predictions = classifiers.knn(dataset)
+	# end = time.time()
+	# elapsed_time = utils.humanize_time(end - start)
+	# print("Elapsed time using knn {0}...".format(elapsed_time))
+	# print("predictions 5x10 = {0}".format(predictions[:][:10]))
+	predictions = [
+		[1, 1, 0, 2, 4, 3, 2, 0, 2, 4, 0, 3, 2, 1, 1],
+		[1, 2, 4, 2, 1, 0, 4, 1, 3, 2, 2, 2, 1, 2, 1],
+		[2, 3, 4, 2, 2, 0, 2, 0, 3, 3, 1, 2, 2, 2, 3],
+ 		[0, 1, 3, 3, 3, 3, 1, 3, 3, 3, 2, 2, 3, 0, 1],
+ 		[3, 0, 2, 1, 4, 2, 1, 0, 2, 4, 1, 1, 4, 2, 3]
+ 	]
+	hist = np.zeros((5, 5), dtype=np.uint32)
 	for i in range(len(predictions)):
 		for j in range(len(predictions[i])):
 			c = predictions[i][j]
 			hist[i][c] += 1
 	print("hist = {0}".format(hist))
+	confusion_matrix = hist / 15.0
+	print("conf mat = \n{0}".format(confusion_matrix))
+	values = [confusion_matrix[i][i] for i in range(5)]
+	precision = np.average(values)
+	print("precision = {0}".format(precision))
+
+	plt.matshow(confusion_matrix)
+	plt.title('Confusion matrix')
+	plt.colorbar()
+	plt.show()
 
 
 if __name__ == '__main__':
